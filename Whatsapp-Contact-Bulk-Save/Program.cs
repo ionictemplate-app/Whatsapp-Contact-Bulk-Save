2e71b95af91e6bd59c5cac880588488ede556e88
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-01-17 21:48:26 +0800

    Add project files.

---
+++ Whatsapp-Contact-Bulk-Save/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whatsapp_Contact_Bulk_Save
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
